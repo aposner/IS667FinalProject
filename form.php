b4fa27ae4381c6b1560e9d197390947151f12056
ava1posner <avaposner128@gmail.com>
2020-12-01 10:20:05 -0500

    form updates

--- form.php
+++ form.php
@@ -5,9 +5,34 @@
 <html>
 <head>
     <title>Maps</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 </head>
 <body>
-<form name="dataInsert" action="index.php?save=pin" method="post">
+    <form name="dataInsert" action="index.php?save=pin" method="post">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Where do you want to go (Name)?</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name" name="name">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Address?</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Address" name="address">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Where do you want to go (Lat)?</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Lat" name="lat">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Where do you want to go (Long)?</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Long" name="lng">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Type?</label>
+        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name" name="type">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+<!-- <form name="dataInsert" action="index.php?save=pin" method="post">
     <table>
         <tr>
             <td>Where do you want to go (Name)?</td>
@@ -34,6 +59,6 @@
             <td><input type="submit" name="submit" value="insert/update"></td>
         </tr>
     </table>
-</form>
+</form> -->
 </body>
 </html>
